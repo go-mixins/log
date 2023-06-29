f9985fa799326435573474ab007a1bf02df69aef
Andrew Rodionoff <andviro@gmail.com>
2023-06-30 00:36:51 +0300

    [pkg] added log wrapper for opencensus trace & span ids

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github.com/go-mixins/log/v2
 
 go 1.20
 
-require golang.org/x/exp v0.0.0-20230626212559-97b1e661b5df
+require (
+	go.opencensus.io v0.24.0
+	golang.org/x/exp v0.0.0-20230626212559-97b1e661b5df
+)
+
+require github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
---
+++ opencensus/handler.go
@@ -0,0 +1,36 @@
+package opencensus
+
+import (
+	"context"
+
+	"go.opencensus.io/trace"
+	"golang.org/x/exp/slog"
+)
+
+type slogHandler struct {
+	next slog.Handler
+}
+
+func (h *slogHandler) Enabled(ctx context.Context, level slog.Level) bool {
+	return h.next.Enabled(ctx, level)
+}
+
+func (h *slogHandler) Handle(ctx context.Context, record slog.Record) error {
+	spanContext := trace.FromContext(ctx).SpanContext()
+	return h.next.WithAttrs([]slog.Attr{
+		slog.String("trace_id", spanContext.TraceID.String()),
+		slog.String("span_id", spanContext.SpanID.String()),
+	}).Handle(ctx, record)
+}
+
+func (h *slogHandler) WithAttrs(attrs []slog.Attr) slog.Handler {
+	return &slogHandler{next: h.next.WithAttrs(attrs)}
+}
+
+func (h *slogHandler) WithGroup(name string) slog.Handler {
+	return &slogHandler{next: h.next.WithGroup(name)}
+}
+
+func Wrap(src slog.Handler) slog.Handler {
+	return &slogHandler{next: src}
+}
